9774bfeb509ba58a56bee80ff61bc6af848c05c2
Max Jones <maxjones203@gmail.com>
2024-03-02 18:43:05 +0000

    Local and Production config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router/router'
+import config from 'env'
 
 const App = () => {
+  console.log('Connfig set to: ' + config.env)
+
   return <RouterProvider router={router} />
 }
 
---
+++ src/env/index.ts
@@ -0,0 +1,17 @@
+import local from './local'
+import production from './production'
+
+const getConfig = () => {
+  if (['localhost', '127.0.0.1'].includes(location.hostname)) {
+    return local
+  }
+  // TODO: Change to production URL
+  if (location.hostname.includes('davinci.com')) {
+    return production
+  }
+  throw new Error("env: There's no config for this environment, please check your env config files.")
+}
+
+const config = getConfig()
+
+export default config
