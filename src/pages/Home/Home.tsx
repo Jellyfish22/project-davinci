6833ea3270fae42b79486a721adbee9370b148c0
Max Jones <maxjones203@gmail.com>
2024-12-16 15:10:24 +0000

    Temp homepage

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,28 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 
 export const Home: FC = () => {
+  const [showText, setShowText] = useState(false);
+
   return (
-    <div className="w-full h-screen flex align-middle text-center justify-center bg-[#27365E]">
-      <div className="text-9xl">Welcome DaVinci</div>
+    <div className="w-full h-screen flex flex-col items-center justify-center relative">
+      <div className="flex items-center justify-center w-full h-full">
+        {showText ? (
+          <div className="text-black text-9xl font-bold">Ern is a cuuuunt</div>
+        ) : (
+          <img
+            src="/retra_full-1.png"
+            alt="Logo"
+            className="max-w-full max-h-full"
+          />
+        )}
+      </div>
+
+      <button
+        className="absolute bottom-5 right-5 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none"
+        onClick={() => setShowText(!showText)}
+      >
+      Goggles Image
+      </button>
     </div>
-  )
-}
+  );
+};
